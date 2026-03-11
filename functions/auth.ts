44575022c3d4211bf91e8321e88d7331b56ff7e2
THEMACHINE <jxs66@163.com>
2026-03-11 16:43:41 +0800

    restore: website from bf317a0

--- functions/auth.ts
+++ functions/auth.ts
@@ -272,11 +272,6 @@ export default {
     try {
       let response: Response;
 
-      // 根路由
-      if (path === '/' && method === 'GET') {
-        return new Response('THEMATHINK API running', { headers: { 'Content-Type': 'text/plain' } });
-      }
-
       // 路由匹配
       if (path === '/api/auth/register' && method === 'POST') {
         response = await handleRegister(request, env);
